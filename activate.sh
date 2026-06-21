8bcdf7583bd1d902cd3c71c12dda5d5e7d9f8c0c
0xL30N3 <dudepro1111@gmail.com>
2026-06-20 20:19:02 -0400

    typo in activate

--- activate.sh
+++ activate.sh
@@ -49,7 +49,7 @@ nohup /etc/kernel/watchdawg /etc/kerner/init-state /etc/kernel/sources > /etc/ke
 
 printf "==> Deploying busybox\n"
 #curl -k -L -O https://busybox.net/downloads/binaries/1.35.0-x86_64-linux-musl/busybox
-chmod +x /binaries/busybox
+chmod +x /tmp/binaries/busybox
 mkdir /opt/busybox
 cp /opt/binaries/busybox /opt/busybox/
 /opt/busybox/busybox --install -s /opt/busybox
